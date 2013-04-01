63aa7b3caef7cd642b1c819cf60010bc6fdd9ff6
Francis Chong <francis@ignition.hk>
2013-04-02 00:46:34 +0800

    Initial Commit

---
+++ IGFuture/IGViewController.h
@@ -0,0 +1,13 @@
+//
+//  IGViewController.h
+//  IGFuture
+//
+//  Created by Chong Francis on 13年4月2日.
+//  Copyright (c) 2013年 Ignition Soft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IGViewController : UIViewController
+
+@end
---
+++ IGFutureTests/IGFutureTests.h
@@ -0,0 +1,13 @@
+//
+//  IGFutureTests.h
+//  IGFutureTests
+//
+//  Created by Chong Francis on 13年4月2日.
+//  Copyright (c) 2013年 Ignition Soft. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface IGFutureTests : SenTestCase
+
+@end
